099a1db41fe9848e8bb48a51e3ad2ae2d0449657
rosindubh <p.welsby2@ntlworld.com>
2021-08-15 22:35:48 +0100

    trying to create an array of the deck of cards

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 // phil welsby - 15 aug 2021
 
+
+//create an array of playing cards
+const deck = new Array();
+deck.src = "./playing-cards-assets/png/";
+
 // variables
 const drawCard = document.getElementById("draw-card");
 const hold = document.getElementById("hold")
 const player1Card1 = document.getElementById("player1-card1");
 
+
 // Hit Me button
 drawCard.addEventListener("click", () => {
-    console.log("Hit Me")
-    let randomNum = Math.floor(Math.random() * 6) +1;
-    //console.log("randomNum =", randomNum);
+    let randomNum = Math.floor(Math.random() * 6);
     if (randomNum == 1) {
-        player1Card1.src = "./playing-cards-assets/png/ace_of_diamonds.png";
-        console.log("randomNum =", randomNum);
+        player1Card1.src = deck[0];
     }
+
 });
+        /*player1Card1.src = "./playing-cards-assets/png/ace_of_diamonds.png";*/
+
+    
+
 
 // hold button
 hold.addEventListener("click", () => {
     console.log("Hold");
 });
 
+deck.drawCard
