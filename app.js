a3dd7d9e2ae3e8d03e79a73ea7cf9e2f4be8fc98
rosindubh <p.welsby2@ntlworld.com>
2021-08-15 23:14:30 +0100

    set up 4 changing aces with [if - else if ] - need to create array of cards!!!

--- app.js
+++ app.js
@@ -14,10 +14,18 @@ const player1Card1 = document.getElementById("player1-card1");
 
 // Hit Me button
 drawCard.addEventListener("click", () => {
-    let randomNum = Math.floor(Math.random() * 6);
+    let randomNum = Math.floor(Math.random() * 4) +1;
     if (randomNum == 1) {
-        //player1Card1.src = "deck[0]";
         player1Card1.src = "./playing-cards-assets/png/ace_of_diamonds.png";
+    }
+    else if (randomNum == 2) {
+        player1Card1.src = "./playing-cards-assets/png/ace_of_clubs.png";
+    }
+    else if (randomNum == 3) {
+        player1Card1.src = "./playing-cards-assets/png/ace_of_hearts.png";
+    }
+    else if (randomNum == 4) {
+        player1Card1.src = "./playing-cards-assets/png/ace_of_spades.png";
     }
 
 });
